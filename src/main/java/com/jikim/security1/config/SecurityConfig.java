20c65ec83c5a85a164448f92f4c3175fa14819e2
Kim Ji Hun <gmldnr2222@naver.com>
2023-04-01 18:34:45 +0900

    feat: 구글 회원 프로필 정보 받아보기

--- src/main/java/com/jikim/security1/config/SecurityConfig.java
+++ src/main/java/com/jikim/security1/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.jikim.security1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,10 +9,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.jikim.security1.config.oauth.PrincipalOauth2UserService;
+
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록이 됨.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured, preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
+
 
 	// 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해줌.
 	@Bean
@@ -32,8 +39,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌. -> controller에 /login을 만들지 않아도 됨.
 			.defaultSuccessUrl("/")
 			.and()
+			//  1. 코드받기(인증)
+			//  2. 엑세스토큰(권한)
+			//  3. 사용자프로필 정보를 가져와서
+			//  4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함.
+			//  4-2. (이메일, 전화번호, 이름, 아이디) 쇼핑몰 -> (집주소), 백화점 -> (vip, 일반등급)
 			.oauth2Login()
-			.loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함.
+			.loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드 X. (엑세스토큰+사용자프로필정보 O)
+			.userInfoEndpoint()
+			.userService(principalOauth2UserService)
 		;
 	}
 }
