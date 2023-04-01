7c30408f081998a72d1f6f9750c9adc35a25cdcd
Kim Ji Hun <gmldnr2222@naver.com>
2023-04-01 17:47:34 +0900

    feat: 구글 로그인 준비 (Oauth2) 및 Jasypt로 암호화

--- src/main/java/com/jikim/security1/config/SecurityConfig.java
+++ src/main/java/com/jikim/security1/config/SecurityConfig.java
@@ -30,6 +30,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 			.loginPage("/loginForm")
 			.loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌. -> controller에 /login을 만들지 않아도 됨.
-			.defaultSuccessUrl("/");
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함.
+		;
 	}
 }
