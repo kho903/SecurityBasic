4709cf6f3b1cfaaeaa9ebd1df151ac4dd4fb19a9
Kim Ji Hun <gmldnr2222@naver.com>
2023-04-02 16:30:22 +0900

    feat: 네이버 로그인 완료

--- src/main/java/com/jikim/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/jikim/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,5 +1,7 @@
 package com.jikim.security1.config.oauth;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.jikim.security1.config.auth.PrincipalDetails;
 import com.jikim.security1.config.oauth.provider.FacebookUserInfo;
 import com.jikim.security1.config.oauth.provider.GoogleUserInfo;
+import com.jikim.security1.config.oauth.provider.NaverUserInfo;
 import com.jikim.security1.config.oauth.provider.OAuth2UserInfo;
 import com.jikim.security1.model.User;
 import com.jikim.security1.repository.UserRepository;
@@ -51,8 +54,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		} else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
 			System.out.println("페이스북 로그인 요청");
 			oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+		} else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			System.out.println("네이버 로그인 요청");
+			oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
 		} else {
-			System.out.println("구글과 페이스북 로그인만 지원합니다.");
+			System.out.println("구글과 페이스북, 네이버 로그인만 지원합니다.");
 		}
 
 		String provider = oAuth2UserInfo.getProvider();
